b22ea68cfb997caa70a644ff1bdf5b9ac89b659c
mifouche <mifouche@mifouche-m.shoprite.co.za>
2015-04-22 12:49:53 +0200

    dbCommunicator

---
+++ src/ahkjava/DBCommunicator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahkjava;
+
+/**
+ *
+ * @author mifouche
+ */
+public class DBCommunicator {
+    
+    /*  LoginUser
+        -usernameExists(username);boolean
+        -usernameMatchPassword(username, password);boolean
+    
+        RegisterUser
+        -usernameExists(username);boolean
+        -emailExists(email);boolean
+        -passwordMatch(password1, password2);boolean
+        -passwordValid(password);boolean
+        -addUser(username, email, password);boolean(for successful adding)
+        
+        FUNCTIONS RELATED TO GAME-POOL
+        getPoolList();String[] (array with all usernames and their userscores)
+        addUserToPool(username);boolean
+        connectToUserInPool(username, opponentUsername);boolean
+    
+        FUNCTIONS RELATED TO GAME-TIME
+    
+    
+    */
+}
