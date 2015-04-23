b6adc054582265cca6151a06c44a37b9a1250502
mifouche <mifouche@mifouche-m.shoprite.co.za>
2015-04-23 22:33:54 +0200

    final merger?

--- src/ahkjava/AHKJava.java
+++ src/ahkjava/AHKJava.java
@@ -611,6 +611,7 @@ public class AHKJava implements ActionListener{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello updater");
         try {
              AHKJava c = new AHKJava();
              c.getRecords();
